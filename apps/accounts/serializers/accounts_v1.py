81cd9196cee881cc60ca1d9fffdc2f3a6554d8a9
K. M. Jiaul Islam Jibon <jiaulislam.ict.bd@gmail.com>
2024-09-24 16:29:03 +0600

    :sparkles: working on Accounts and Authentication

--- apps/accounts/serializers/accounts_v1.py
+++ apps/accounts/serializers/accounts_v1.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers as s
+from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 
 
 class AccountSerializerV1(s.ModelSerializer):
@@ -23,3 +25,19 @@ class AccountSerializerV1(s.ModelSerializer):
             "updated_by",
             "is_superuser",
         ]
+
+
+class LoginSerializer(s.Serializer):
+    email = s.EmailField()
+    password = s.CharField(style={"input_type": "password"}, write_only=True)
+
+
+class CookieTokenRefreshSerializer(TokenRefreshSerializer):
+    refresh = None
+
+    def validate(self, attrs):
+        attrs["refresh"] = self.context["request"].COOKIES.get("refresh")
+        if attrs["refresh"]:
+            return super().validate(attrs)
+        else:
+            raise InvalidToken("No valid token found in cookie 'refresh'")
--- apps/accounts/urls/urls_v1.py
+++ apps/accounts/urls/urls_v1.py
@@ -1,9 +1,29 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from ..views.views_v1 import AccountViewSetV1
+from ..views.views_v1 import (
+    AccountViewSetV1,
+    CookieTokenRefreshView,
+    LoginView,
+    LogoutView,
+    MeView,
+)
 
 router = DefaultRouter()
 
 router.register("accounts", AccountViewSetV1, basename="accounts")
 
 urlpatterns = router.urls
+
+urlpatterns.extend(
+    [
+        path("login/", LoginView.as_view(), name="auth-login"),
+        path("logout/", LogoutView.as_view(), name="auth-logout"),
+        path("me/", MeView.as_view(), name="auth-me"),
+        path(
+            "refresh-token/",
+            CookieTokenRefreshView.as_view(),
+            name="auth-refresh-token",
+        ),
+    ]
+)
