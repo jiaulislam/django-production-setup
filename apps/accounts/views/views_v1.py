016828d9f0fe08182651cbe06eb6bf1565326438
K. M. Jiaul Islam Jibon <jiaulislam.ict.bd@gmail.com>
2024-09-24 16:59:06 +0600

    :bug: fixing bug for cookie token refresh view

--- apps/accounts/views/views_v1.py
+++ apps/accounts/views/views_v1.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth import authenticate, get_user_model
 from rest_framework import exceptions as e
 from rest_framework import response as r
@@ -60,12 +61,12 @@ class CookieTokenRefreshView(TokenRefreshView):
     def finalize_response(self, request, response, *args, **kwargs):
         if response.data.get("refresh"):
             response.set_cookie(
-                key=s.SIMPLE_JWT["AUTH_COOKIE_REFRESH"],
+                key=settings.SIMPLE_JWT["AUTH_COOKIE_REFRESH"],
                 value=response.data["refresh"],
-                expires=s.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"],
-                secure=s.SIMPLE_JWT["AUTH_COOKIE_SECURE"],
-                httponly=s.SIMPLE_JWT["AUTH_COOKIE_HTTPONLY"],
-                samesite=s.SIMPLE_JWT["AUTH_COOKIE_SAMESITE"],
+                expires=settings.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"],
+                secure=settings.SIMPLE_JWT["AUTH_COOKIE_SECURE"],
+                httponly=settings.SIMPLE_JWT["AUTH_COOKIE_HTTPONLY"],
+                samesite=settings.SIMPLE_JWT["AUTH_COOKIE_SAMESITE"],
             )
         del response.data["refresh"]
 
